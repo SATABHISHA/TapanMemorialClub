a25e95561329fcb47115f19bc68ec5ba0f659ccb
SATABHISHA <satabhisha.innits@gmail.com>
2026-06-10 06:41:27 +0530

    Revert "modified"
    This reverts commit c3a5ca3824dbafbebb7bae2b518ba73c6c029aa6.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -56,9 +56,8 @@ class HomeController extends Controller
             ->limit(6)
             ->get();
         $historyPage = DynamicPage::query()
-            ->whereIn('slug', ['history-of-the-club', 'our-journey'])
+            ->where('slug', 'history-of-the-club')
             ->where('is_published', true)
-            ->orderByRaw("CASE WHEN slug = 'history-of-the-club' THEN 0 WHEN slug = 'our-journey' THEN 1 ELSE 2 END")
             ->first();
 
         $introText = $historyPage
